66e837ada28198560e1552be8fb6ebff02d57319
dudenheini <janlucas.uslu@gmail.com>
2021-03-12 20:24:43 +0100

    Cleanup

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 import os
 from flask import Flask, jsonify, request
-from flask_restful import Resource ,Api, reqparse
+from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
-
-import matplotlib.pyplot as plt
-
 import numpy as np
 import tensorflow as tf
 
@@ -13,10 +10,10 @@ CORS(app)
 api = Api(app)
 
 app_dir = os.path.dirname(os.path.realpath(__file__))
-CNN_model_path = os.path.join(app_dir, "Mnist", "CNN" , "mnist_CNN_model.h5")
-DNN_model_path = os.path.join(app_dir, "Mnist", "DNN" , "mnist_DNN_model.h5")
-GAN_model_path = os.path.join(app_dir, "Mnist", "GAN" , "mnist_GAN_model.h5")
-DCGAN_model_path = os.path.join(app_dir, "Mnist", "GAN" , "mnist_DCGAN_model.h5")
+CNN_model_path = os.path.join(app_dir, "Mnist", "CNN", "mnist_CNN_model.h5")
+DNN_model_path = os.path.join(app_dir, "Mnist", "DNN", "mnist_DNN_model.h5")
+GAN_model_path = os.path.join(app_dir, "Mnist", "GAN", "mnist_GAN_model.h5")
+DCGAN_model_path = os.path.join(app_dir, "Mnist", "GAN", "mnist_DCGAN_model.h5")
 
 print("Initialising Models")
 CNN_Model = tf.keras.models.load_model(CNN_model_path)
@@ -25,48 +22,58 @@ GAN_Model = tf.keras.models.load_model(GAN_model_path, compile=False)
 DCGAN_Model = tf.keras.models.load_model(DCGAN_model_path, compile=False)
 print("Models ready")
 
+
 class MnistClassifier(Resource):
     def get(self):
-        return {"Message" : "This is the MNIST Classifier Network"}, 200
+        return {"Message": "This is the MNIST Classifier Network"}, 200
 
-    def post(self):   
+    def post(self):
         reqData = request.get_json()
 
         if "data" in reqData:
             Data = reqData["data"]
         else:
             Data = None
-        
-        if Data and isinstance(Data,list) and np.array(Data).shape == (28,28):
+
+        if Data and isinstance(Data, list) and np.array(Data).shape == (28, 28):
             X = np.array(Data)
 
             # Expanding dims from (rows,cols) to (samples, rows , cols , channels)
-            X_CNN = np.expand_dims(np.expand_dims(X,axis=-1),axis=0)
+            X_CNN = np.expand_dims(np.expand_dims(X, axis=-1), axis=0)
 
             # Flatten the Array for the DNN
-            X_DNN = np.expand_dims(X.flatten(),axis=0)
-            
+            X_DNN = np.expand_dims(X.flatten(), axis=0)
+
             #  Predicting
             pred_vector_CNN = np.array(CNN_Model.predict(X_CNN))
             pred_vector_DNN = np.array(DNN_Model.predict(X_DNN))
-            pred_CNN = { "number" : int(np.argmax(pred_vector_CNN)) , "prob" : int(np.round(np.amax(pred_vector_CNN),2)*100) }
-            pred_DNN = { "number" : int(np.argmax(pred_vector_DNN)) , "prob" : int(np.round(np.amax(pred_vector_DNN),2)*100) }
+            pred_CNN = {
+                "number": int(np.argmax(pred_vector_CNN)),
+                "prob": int(np.round(np.amax(pred_vector_CNN), 2) * 100),
+            }
+            pred_DNN = {
+                "number": int(np.argmax(pred_vector_DNN)),
+                "prob": int(np.round(np.amax(pred_vector_DNN), 2) * 100),
+            }
 
             resData = {
-                "message" : "Prediction complete",
-                "pred_vector_CNN" : pred_vector_CNN.tolist(),
-                "pred_CNN" : pred_CNN,
-                "pred_vector_DNN" : pred_vector_DNN.tolist(),
-                "pred_DNN" : pred_DNN,
+                "message": "Prediction complete",
+                "pred_vector_CNN": pred_vector_CNN.tolist(),
+                "pred_CNN": pred_CNN,
+                "pred_vector_DNN": pred_vector_DNN.tolist(),
+                "pred_DNN": pred_DNN,
             }
             return resData, 200
-        return { "message" : "You need to pass 'data' as an array in the shape of (28,28) of with all entries need to be Ints or Floats between 0 and 255"}, 400
+        return {
+            "message": "You need to pass 'data' as an array in the shape of (28,28) of with all entries need to be Ints or Floats between 0 and 255"
+        }, 400
+
 
 class MnistGenerator(Resource):
     def get(self):
-        return {"Message" : "This is the MNIST Generator Network"}, 200
+        return {"Message": "This is the MNIST Generator Network"}, 200
 
-    def post(self):   
+    def post(self):
         reqData = request.get_json()
         minVal = 100
         maxVal = -100
@@ -75,15 +82,15 @@ class MnistGenerator(Resource):
             LV = reqData["latentVector"]
         else:
             LV = None
-        
-        if LV and isinstance(LV,list) and np.array(LV).shape == (10,):
+
+        if LV and isinstance(LV, list) and np.array(LV).shape == (10,):
             LV = np.array(LV)
 
             # Expanding dims from (rows,cols) to (samples, rows , cols , channels)
-            LV = np.expand_dims(LV,axis=0)
+            LV = np.expand_dims(LV, axis=0)
             # Generating
-            GANImg = np.reshape(np.array(GAN_Model.predict(LV)),(28,28))
-            DCGANImg = np.reshape(np.array(DCGAN_Model.predict(LV)),(28,28))
+            GANImg = np.reshape(np.array(GAN_Model.predict(LV)), (28, 28))
+            DCGANImg = np.reshape(np.array(DCGAN_Model.predict(LV)), (28, 28))
 
             # get the values between 0 and 1
             maxVal = np.amax(GANImg)
@@ -97,13 +104,16 @@ class MnistGenerator(Resource):
             DCGANImg = (DCGANImg - minVal) / deltaValue
 
             resData = {
-                "message" : "Generation complete",
-                "GANImage" : GANImg.tolist(),
-                "DCGANImage" : DCGANImg.tolist()
+                "message": "Generation complete",
+                "GANImage": GANImg.tolist(),
+                "DCGANImage": DCGANImg.tolist(),
             }
             return resData, 200
 
-        return { "message" : "You need to pass 'latentVector' as an array in the shape of (10) of with all entries need to be Floats between 0 and 1"}, 400
+        return {
+            "message": "You need to pass 'latentVector' as an array in the shape of (10) of with all entries need to be Floats between 0 and 1"
+        }, 400
+
 
 api.add_resource(MnistClassifier, "/api/ML/mnist/classifier")
 api.add_resource(MnistGenerator, "/api/ML/mnist/generator")
